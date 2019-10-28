9cd664ecb5a213472f4d1f012e212b9424308abf
Raksit <raksitzaza@gmail.com>
2019-10-28 20:58:14 +0700

    Update CPP_lab10_1.cpp

--- CPP_lab10_1.cpp
+++ CPP_lab10_1.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
-#include <string>
-using namespace std;
 class Hero {
-    private:
-        string name ;
-        int level ;
-
-    public:
-        Hero (string n , int num) {
-            name = n ;
-            level = num ;
-        }
-
-    string getName() {
-        return name ;
+  private:
+    string name;
+    int level;
+  public:
+    Hero (string n , int num){
+      name = n;
+      level = num;
     }
-
-    int getlevel() {
-        return level ;
-    }
+  string getName() {
+    return name;
+  }
+ 
+  int getLevel() {
+    return level;
+  }
 };
