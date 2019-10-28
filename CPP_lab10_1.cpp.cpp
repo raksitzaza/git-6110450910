955c5301ae55886e7c0eb24988b8685d7f884658
nattapononc <nattapon.onc@ku.th>
2019-10-28 14:50:00 +0700

    Lab10_2

--- CPP_lab10_1.cpp.cpp
+++ CPP_lab10_1.cpp.cpp
@@ -1,22 +1,3 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Hero {
-    private:
-        string name ;
-        int level ;
-
-    public:
-        Hero (string n , int num) {
-            name = n ;
-            level = num ;
-        }
-
-    string getName() {
-        return name ;
-    }
-
-    int getlevel() {
-        return level ;
-    }
-};
